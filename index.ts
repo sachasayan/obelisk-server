8d81d9ca0e3db56b848aa7c205c1aa6c6058e357
Sacha Sayan <sacha.sayan@rangle.io>
2020-02-17 16:33:48 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const wait = (t: number) => new Promise(ok => setTimeout(ok, t));
 (async () => {
   try {
 
-  console.log('Init..2');
+  console.log('Init..');
 
 
   const matrix = new LedMatrix(
@@ -29,13 +29,11 @@ const wait = (t: number) => new Promise(ok => setTimeout(ok, t));
   matrix
     .brightness(100)
     .fgColor(0xFFFFFF)
-    .fill();
-
-  matrix
+    .fill()
     .sync();
 
 
-  await wait(2);
+  await wait(200);
 }
 catch (error) {
   console.error(`${__filename} caught: `, error);
