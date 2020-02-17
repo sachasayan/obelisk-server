aae0c33887b7eaa69d9c13465589f5561fcceb19
Sacha Sayan <sacha.sayan@rangle.io>
2020-02-17 17:35:39 -0500

    Initial Pac Man

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ import {
   RuntimeOptions,
 } from 'rpi-led-matrix';
 
+import { Pac } from './pac';
+
 
 export const matrixOptions: MatrixOptions = {
   ...LedMatrix.defaultMatrixOptions(),
@@ -29,32 +31,26 @@ export const runtimeOptions: RuntimeOptions = {
 };
 
 
+let f = Pac.field.replace(/\n|\r/g, "");
+let o = [...f].map((char) => { Pac.colors[char]});
+
+
 const wait = (t: number) => new Promise(ok => setTimeout(ok, t));
 
 (async () => {
   try {
     const matrix = new LedMatrix(matrixOptions, runtimeOptions);
-    matrix
-    .brightness(20)
-      .fgColor(0xFFFFFF)
-      .fill()
-      .sync();
 
     matrix
-      .clear()            // clear the display
-      .brightness(100)    // set the panel brightness to 100%
-      .fgColor(0x0000FF)  // set the active color to blue
-      .fill()             // color the entire diplay blue
-      .fgColor(0xFFFF00)  // set the active color to yellow
-      // draw a yellow circle around the display
-      .drawCircle(matrix.width() / 2, matrix.height() / 2, matrix.width() / 2 - 1)
-      // draw a yellow rectangle
-      .drawRect(matrix.width() / 4, matrix.height() / 4, matrix.width() / 2, matrix.height() / 2)
-      // sets the active color to red
-      .fgColor({ r: 255, g: 0, b: 0 })
-      // draw two diagonal red lines connecting the corners
-      .drawLine(0, 0, matrix.width(), matrix.height())
-      .drawLine(matrix.width() - 1, 0, 0, matrix.height() - 1);
+      .clear()
+      .brightness(100);
+
+    o.forEach((c, i) => {
+      matrix.fgColor(c)
+        .setPixel(i % 128, Math.floor(i / 128))
+
+
+    });
 
     matrix.sync();
 
@@ -68,3 +64,21 @@ const wait = (t: number) => new Promise(ok => setTimeout(ok, t));
 
 
 
+
+
+
+// matrix
+// .clear()            // clear the display
+// .brightness(100)    // set the panel brightness to 100%
+// .fgColor(0x0000FF)  // set the active color to blue
+// .fill()             // color the entire diplay blue
+// .fgColor(0xFFFF00)  // set the active color to yellow
+// // draw a yellow circle around the display
+// .drawCircle(matrix.width() / 2, matrix.height() / 2, matrix.width() / 2 - 1)
+// // draw a yellow rectangle
+// .drawRect(matrix.width() / 4, matrix.height() / 4, matrix.width() / 2, matrix.height() / 2)
+// // sets the active color to red
+// .fgColor({ r: 255, g: 0, b: 0 })
+// // draw two diagonal red lines connecting the corners
+// .drawLine(0, 0, matrix.width(), matrix.height())
+// .drawLine(matrix.width() - 1, 0, 0, matrix.height() - 1);
