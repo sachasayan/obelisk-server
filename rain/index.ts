731883486a6e1c5f382708ba0b02ba37a3e3bec6
Sacha Sayan <sacha.sayan@rangle.io>
2020-02-18 19:09:49 -0500

    Update index.ts

--- rain/index.ts
+++ rain/index.ts
@@ -9,11 +9,11 @@ class Raindrop {
     velocity: number,
   ) {
     this.velocity = velocity;
-    this.y = y + Math.random() * 10;
+    this.y = y + Math.random() * 16;
   }
 
   next(): number {
-    // this.y += this.velocity;
+    this.y += this.velocity;
     this.velocity += 0.01;
     if (this.y > 16) {
       this.y = 0;
@@ -31,7 +31,7 @@ function initRain (matrix){
     const raindrops: Raindrop[] = [];
 
     for (let x = 0; x < matrix.width()/2 ; x++) {
-      raindrops.push(new Raindrop(Math.random()*matrix.width(), 0, 0));
+      raindrops.push(new Raindrop(Math.random()*matrix.width(), 0, 0.2));
     }
 
     matrix.afterSync((mat, dt, t) => {
