de9365463ecfdd0a7baf897c4999d57b10ec475e
Sacha Sayan <sacha.sayan@rangle.io>
2020-02-20 02:10:16 -0500

    Update index.ts

--- rain/index.ts
+++ rain/index.ts
@@ -77,7 +77,7 @@ function init (matrix){
       if(rocket){
         rocket.scan(0, 0, rocket.bitmap.width, rocket.bitmap.height, function(x, y, idx) {
           let pc = rocket.getPixelColor(x, y);
-          if (pc % 256 != 255) {
+          if (pc % 256 == 255) {
             matrix.fgColor( (pc - (pc % 256)) / 256).setPixel(x + (64-8), y);
           }
         });
