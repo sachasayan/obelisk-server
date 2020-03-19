c31957e7d9de24a8d776d736f101c24c04993657
Sacha Sayan <sacha.sayan@rangle.io>
2020-03-19 17:57:44 -0400

    Update index.ts

--- munchman/index.ts
+++ munchman/index.ts
@@ -8,7 +8,7 @@ const baseColors = {
   'pinky':  0x00FFFF,
   'clyde':  0xFFB852,
   'empty':  0x000000,
-  'dot':    0x111111
+  'dot':    0x0A0A0A
 };
 
 let colors = {
@@ -176,7 +176,7 @@ function displayGameScreen(t: number){
   gameState.field.forEach((row, y) => {
     row.forEach((c, x) => {
     matrix
-      .fgColor(c === 'D' ? colors.dot(fade(t, 1000, (x / 128) )).num() : baseColors[TILES[c]])
+      .fgColor(c === 'D' ? colors.dot(fade(t, 3000, (x / 128) )).num() : baseColors[TILES[c]])
       .setPixel(x, y);
     });
   });
