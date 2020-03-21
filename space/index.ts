c8ed6eb0c4d574cf5d869d0f0b5aaebc24bd4b73
Sacha Sayan <sacha.sayan@rangle.io>
2020-03-21 18:35:44 -0400

    spaceship

--- space/index.ts
+++ space/index.ts
@@ -3,36 +3,39 @@ import * as chroma from 'chroma-js';
 
 
 const palette = chroma.scale([0xFFFFFF, 0x111111]);
+let matrix;
 
 class Star {
   distance: number; // Distance (from camera) affects brightness and speed
   y: number;
+  x: number;
 
   constructor(
-    readonly x: number,
+    x: number,
     distance: number,
   ) {
-    this.distance = distance;
+    this.distance = 1 + Math.random()*10
+    this.x = Math.random()* matrix.width()
     this.y = Math.random() * 16;
   }
 
   step() {
-    this.y += 3 / this.distance;
+    this.y += 1 / this.distance;
     if (this.y > 16) {
       this.y -= 32;
     }
   }
 }
 
-function init (matrix){
-
+function init (m){
+    matrix = m;
     matrix.clear();
 
     let rocket;
     const starfield: Star[] = [];
 
     for (let x = 0; x < matrix.width() * 2 ; x++) {
-      starfield.push(new Star(Math.random()*matrix.width(), 1 + Math.random()*10));
+      starfield.push(new Star());
     }
 
     matrix.afterSync((mat, dt, t) => {
@@ -44,14 +47,14 @@ function init (matrix){
         matrix.fgColor(palette(star.distance/10).num()).setPixel(star.x, Math.floor(star.y));
       });
 
-      // if(rocket){
-      //   rocket.scan(0, 0, rocket.bitmap.width, rocket.bitmap.height, function(x, y, idx) {
-      //     let pc = rocket.getPixelColor(x, y);
-      //     if (pc % 256 == 255) {
-      //       matrix.fgColor( (pc - (pc % 256)) / 256).setPixel( x + (64-8), y);
-      //     }
-      //   });
-      // }
+      if(rocket){
+        rocket.scan(0, 0, rocket.bitmap.width, rocket.bitmap.height, function(x, y, idx) {
+          let pc = rocket.getPixelColor(x, y);
+          if (pc % 256 == 255) {
+            matrix.fgColor( (pc - (pc % 256)) / 256).setPixel( x + (64-8), y);
+          }
+        });
+      }
 
       setTimeout(() => matrix.sync(), 40);
     });
