be249482697c870249870b3c30cc3031cd0cdb61
Sacha Sayan <sacha.sayan@rangle.io>
2020-03-21 21:45:37 -0400

    Update index.ts

--- space/index.ts
+++ space/index.ts
@@ -53,7 +53,7 @@ function init (m){
               .setPixel(
                 x +
                   (64 - 8) +
-                  Math.round(5 * Math.sin(1 * Math.PI * (t/1000))), // + // Freq: 1/s, Spread: 5px
+                  Math.round(5 * Math.sin(5 * Math.PI * (t/1000))), // + // Freq: 1/s, Spread: 5px
                 y +
                   1 +
                   Math.round(1 * Math.sin(2 * Math.PI * (t/1000))) // Freq: 2/s, Spread: 3px
