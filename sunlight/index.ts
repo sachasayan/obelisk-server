4b734c6ce7bd27c0953ac21a3610e4d0ab92d5e3
Sacha Sayan <sacha.sayan@rangle.io>
2020-03-23 19:46:12 -0400

    Update index.ts

--- sunlight/index.ts
+++ sunlight/index.ts
@@ -1,27 +1,19 @@
 
-
-const wait = (t: number) => new Promise(ok => setTimeout(ok, t));
-
 function init (matrix){
-  (async () => {
-      matrix.clear();
+  matrix.clear();
+  matrix.afterSync((mat, dt, t) => {
+    matrix.clear();
 
-      matrix
-        .clear()            // clear the display
-        .brightness(100)    // set the panel brightness to 100%
-        .fgColor(0xffcccc)  // set the active color to blue
-        .fill()             // color the entire diplay blue
-        .sync();
+    matrix
+      .clear()            // clear the display
+      .brightness(100)    // set the panel brightness to 100%
+      .fgColor(0xffcccc)  // set the active color to blue
+      .fill();             // color the entire diplay blue
 
-      matrix.sync();
-      await wait(999999999);
-  })();
-};
+    setTimeout(() => matrix.sync(), 0);
+  });
+}
 
 let Sunlight = { init };
 
-
 export { Sunlight };
-
-
-
