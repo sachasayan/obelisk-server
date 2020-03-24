63aa7afa541e5531f95aba51d5766cfc2c82ce93
Sacha Sayan <sacha.sayan@rangle.io>
2020-03-23 20:02:51 -0400

    Update index.ts

--- sunlight/index.ts
+++ sunlight/index.ts
@@ -7,7 +7,7 @@ function init (matrix){
     matrix
       .clear()            // clear the display
       .brightness(100)    // set the panel brightness to 100%
-      .fgColor(0xCCCCCC)  // set the active color to blue
+      .fgColor(0xCCDDCC)  // set the active color to blue
       .fill();             // color the entire diplay blue
 
     setTimeout(() => matrix.sync(), 0);
