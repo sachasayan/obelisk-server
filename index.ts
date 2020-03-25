6676379f2acd1b8d80c59328d48ff2ca24ca7f84
Sacha Sayan <sacha.sayan@rangle.io>
2020-03-25 03:11:51 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -13,6 +13,8 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 let players = [];
 let playerData = [];
 
