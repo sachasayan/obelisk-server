0ed5f142f1b2934846a44cbe830a1f0ae8c7e87d
Sacha Sayan <sacha.sayan@rangle.io>
2020-03-27 02:13:29 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -26,7 +26,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const matrixOptions: MatrixOptions = {
   ...LedMatrix.defaultMatrixOptions(),
